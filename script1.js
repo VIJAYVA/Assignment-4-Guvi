bbfe1d4a8d00f39f3b53ddc0bf8c65e55f9fe344
VIJAY ANAND R <97502792+VIJAYVA@users.noreply.github.com>
2022-03-03 17:39:57 +0530

    Add files via upload

--- script1.js
+++ script1.js
@@ -1,8 +1,8 @@
-var request = new XMLHttpRequest();
+var request=new XMLHttpRequest();
 request.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
 request.send();
 request.onload=function(){
     var data=JSON.parse(request.response);
     for(i=0;i<data.length;i++)
-    console.log(`flag:${data[i].flag}`);
+    console.log(`counries name:${data[i].name} region:${data[i].region} sub region:${data[i].subregion} population:${data[i].population}`);
 }
